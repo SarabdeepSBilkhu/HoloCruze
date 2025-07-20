6981041cb41972ee02385582a6ccf260bfa7a107
Sarabdeep Singh Bilkhu <117672580+SarabdeepSBilkhu@users.noreply.github.com>
2025-07-20 16:58:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/CampusSync/" : "/",
+  base: mode === "production" ? "/HoloCruze/" : "/",
   server: {
     host: "::",
     port: 8080,
