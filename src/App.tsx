e0c41a7a279130799a387550987b527ca9db41cb
Sarabdeep Singh Bilkhu <117672580+SarabdeepSBilkhu@users.noreply.github.com>
2025-07-20 18:06:21 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/HoloCruze">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/car" element={<Car />} />
